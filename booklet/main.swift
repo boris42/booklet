b5c0bd27310b94e153ef6d0bf5f9a510e89f76b9
Boris Herman <boris42@mac.com>
2019-10-15 21:33:19 +0200

    Initial Commit

---
+++ booklet/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  booklet
+//
+//  Created by Boris Herman on 15/10/2019.
+//  Copyright © 2019 Sight&Sound s.p. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
